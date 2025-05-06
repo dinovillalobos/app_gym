8a24bca4ede1858b4a9e07980c096b9707f7cd83
dinovillalobos <villalobosdoricardo@gmail.com>
2025-05-05 18:07:28 -0600

    agregando web al firebase options

--- lib/firebase_options.dart
+++ lib/firebase_options.dart
@@ -17,10 +17,7 @@ import 'package:flutter/foundation.dart'
 class DefaultFirebaseOptions {
   static FirebaseOptions get currentPlatform {
     if (kIsWeb) {
-      throw UnsupportedError(
-        'DefaultFirebaseOptions have not been configured for web - '
-        'you can reconfigure this by running the FlutterFire CLI again.',
-      );
+      return web;
     }
     switch (defaultTargetPlatform) {
       case TargetPlatform.android:
@@ -65,4 +62,15 @@ class DefaultFirebaseOptions {
     storageBucket: 'gymhibrido-9d221.firebasestorage.app',
     iosBundleId: 'com.example.appGymHibrido',
   );
+
+  static const FirebaseOptions web = FirebaseOptions(
+    apiKey: 'AIzaSyD7ubv5hB-scNn66Bjg4UhSLdpZhhC_LpE',
+    appId: '1:828312872083:web:3c751c6f15a715f106d966',
+    messagingSenderId: '828312872083',
+    projectId: 'gymhibrido-9d221',
+    authDomain: 'gymhibrido-9d221.firebaseapp.com',
+    storageBucket: 'gymhibrido-9d221.firebasestorage.app',
+    measurementId: 'G-VFL9DTRXD0',
+  );
+
 }
