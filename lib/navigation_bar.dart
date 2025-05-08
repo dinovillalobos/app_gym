93433c217fd3a5e960f949f200c7d98ab823c671
YaelDuarte <yaelduarte48@aragon.unam.mx>
2025-05-08 17:50:21 -0600

    Uso de librerio externa de flutter para el uso de icono de mancuerna, se agrego al yml y diseño de la Navigation Bar

--- lib/navigation_bar.dart
+++ lib/navigation_bar.dart
@@ -1,65 +1,89 @@
 import 'package:flutter/material.dart';
 import 'package:app_gym_hibrido/screens/home_screen.dart';
 import 'package:app_gym_hibrido/screens/crear_rutina_screen.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 
-class navigationBar extends StatefulWidget{
+class navigationBar extends StatefulWidget {
   final String userId;
-  const navigationBar ({super.key, required this.userId});
+  const navigationBar({super.key, required this.userId});
 
   @override
   State<navigationBar> createState() => _navigationBarState();
 }
 
-class _navigationBarState extends State<navigationBar>{
+class _navigationBarState extends State<navigationBar> {
   int _screenAct = 0;
   late Widget _cuerpo;
 
-  void _changeScreen(int i){
+  void _changeScreen(int i) {
     setState(() {
       _screenAct = i;
 
       switch (_screenAct) {
         case 0:
           _cuerpo = HomeScreen(title: "Inicio");
+          break;
         case 1:
           _cuerpo = CrearRutinaScreen(
             title: "Rutinas",
             userId: widget.userId,
           );
+          break;
         default:
           _cuerpo = HomeScreen(title: "Inicio");
-
       }
     });
   }
 
   @override
-  void initState(){
+  void initState() {
     super.initState();
     _cuerpo = HomeScreen(title: "Inicio");
   }
 
   @override
-  Widget build(BuildContext context){
+  Widget build(BuildContext context) {
     return Scaffold(
       body: _cuerpo,
-      bottomNavigationBar: BottomNavigationBar(
-          items: const[
-            BottomNavigationBarItem(
-                icon: Icon(Icons.home),
-                label: 'Inicio',
+      bottomNavigationBar: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: Container(
+          decoration: BoxDecoration(
+            color: Colors.black,
+            borderRadius: BorderRadius.circular(30),
+            boxShadow: [
+              BoxShadow(
+                color: Colors.black26,
+                blurRadius: 10,
+                offset: Offset(0, 4),
+              ),
+            ],
+          ),
+          child: ClipRRect(
+            borderRadius: BorderRadius.circular(30),
+            child: BottomNavigationBar(
+              items: const [
+                BottomNavigationBarItem(
+                  icon: Icon(Icons.home),
+                  label: 'Inicio',
+                ),
+                BottomNavigationBarItem(
+                  icon: FaIcon(FontAwesomeIcons.dumbbell),
+                  label: 'Rutinas',
+                ),
+              ],
+              backgroundColor: Colors.black,
+              currentIndex: _screenAct,
+              selectedItemColor: Colors.blue,
+              unselectedItemColor: Colors.white,
+              onTap: _changeScreen,
+              type: BottomNavigationBarType.fixed,
+              elevation: 0,
             ),
-            BottomNavigationBarItem(
-                icon: Icon(Icons.sports_bar),
-                label: 'Rutinas',
-            ),
-          ],
-        backgroundColor: Colors.black,
-        currentIndex: _screenAct,
-        selectedItemColor: Colors.blue,
-        unselectedItemColor: Colors.white,
-        onTap: _changeScreen,
+          ),
+        ),
       ),
     );
   }
 }
+
